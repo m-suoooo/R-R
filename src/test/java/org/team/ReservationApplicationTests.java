22046221674a6520dfbcaf18a24e36fde457c849
yonggyo1125 <yonggyo00@kakao.com>
2023-04-17 13:22:05 +0900

    초기 셋팅

---
+++ src/test/java/org/team/ReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.team;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
